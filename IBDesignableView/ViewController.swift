1c460aab4317de6e9df25e20d07b3c8047888e82
Suraphan 'Rawd' Laokondee <Suraphan.L@kaidee.com>
2016-08-08 23:10:14 +0700

    Initial Commit

---
+++ IBDesignableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IBDesignableView
+//
+//  Created by Suraphan 'Rawd' Laokondee on 8/8/2559 BE.
+//  Copyright © 2559 rawd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ IBDesignableViewTests/IBDesignableViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  IBDesignableViewTests.swift
+//  IBDesignableViewTests
+//
+//  Created by Suraphan 'Rawd' Laokondee on 8/8/2559 BE.
+//  Copyright © 2559 rawd. All rights reserved.
+//
+
+import XCTest
+@testable import IBDesignableView
+
+class IBDesignableViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
